5905e5003b7ead92efb172074121e6032ba9d20e
Andres Recalde <andres.recalde@lacolombe.net>
2017-01-09 13:34:07 -0500

    removed entry info from gl

--- models/file_uploads/whole_foods.sql
+++ models/file_uploads/whole_foods.sql
@@ -7,25 +7,25 @@
 }}
 
 
-select md5(start_date || store_number || upc) as unique_key, start_date, end_date, region, store_name, store_status, store_number, category, class, sub_category, lpad(upc, 12, '0') as upc, name, volume, units, net_sales, gross_sales, quantity
+select md5(start_date || store_number || upc) as unique_key, start_date, end_date, region, store_name, store_status, store_number, category, class, sub_category, lpad(upc, 12, '0') as upc, name, volume, units, replace(net_sales, ',', '')::decimal(16,2) as net_sales, replace(gross_sales, ',', '')::decimal(16,2) as gross_sales, quantity
 from google_sheets.whole_foods
 
 union 
 
-select md5(start_date || store_number || upc) as unique_key, start_date, end_date, region, store_name, store_status, store_number, category, class, sub_category, lpad(upc, 12, '0') as upc, name, volume, units, net_sales, gross_sales, quantity
+select md5(start_date || store_number || upc) as unique_key, start_date, end_date, region, store_name, store_status, store_number, category, class, sub_category, lpad(upc, 12, '0') as upc, name, volume, units, replace(net_sales, ',', '')::decimal(16,2) as net_sales, replace(gross_sales, ',', '')::decimal(16,2) as gross_sales, quantity
 from google_sheets.whole_foods_2014
 
 union 
 
-select md5(start_date || store_number || upc) as unique_key, start_date, end_date, region, store_name, store_status, store_number, category, class, sub_category, lpad(upc, 12, '0') as upc, name, volume, units, net_sales, gross_sales, quantity
+select md5(start_date || store_number || upc) as unique_key, start_date, end_date, region, store_name, store_status, store_number, category, class, sub_category, lpad(upc, 12, '0') as upc, name, volume, units, net_sales::decimal(16,2), gross_sales::decimal(16,2), quantity
 from google_sheets.whole_foods_2015
 
 union 
 
-select md5(start_date || store_number || upc) as unique_key, start_date, end_date, region, store_name, store_status, store_number, category, class, sub_category, lpad(upc, 12, '0') as upc, name, volume, units, net_sales, gross_sales, quantity
+select md5(start_date || store_number || upc) as unique_key, start_date, end_date, region, store_name, store_status, store_number, category, class, sub_category, lpad(upc, 12, '0') as upc, name, volume, units, net_sales::decimal(16,2), gross_sales::decimal(16,2), quantity
 from google_sheets.whole_foods_2016_1
 
 union 
 
-select md5(start_date || store_number || upc) as unique_key, start_date, end_date, region, store_name, store_status, store_number, category, class, sub_category, lpad(upc, 12, '0') as upc, name, volume, units, net_sales, gross_sales, quantity
+select md5(start_date || store_number || upc) as unique_key, start_date, end_date, region, store_name, store_status, store_number, category, class, sub_category, lpad(upc, 12, '0') as upc, name, volume, units, net_sales::decimal(16,2), gross_sales::decimal(16,2), quantity
 from google_sheets.whole_foods_2016_2
