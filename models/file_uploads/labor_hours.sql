d6c509551439edf472952bb87e7b0077d48efa24
Andres Recalde <andres.recalde@lacolombe.net>
2017-02-01 19:23:38 -0500

    fixed retail labor hours

--- models/file_uploads/labor_hours.sql
+++ models/file_uploads/labor_hours.sql
@@ -4,6 +4,6 @@ l.start_time,
 l.end_time, 
 l.home_cost_center, 
 l.worked_cost_center, 
-t.title as employee_title,
+t.title as employee_title
 from file_uploads.cafe_labor_hours l
 left join file_uploads.employee_titles t on t.employee_code = l.employee_code
--- models/tables/retail/retail_labor_hours.sql
+++ models/tables/retail/retail_labor_hours.sql
@@ -8,10 +8,7 @@ select distinct md5(l.employee_code || l.start_time || l.end_time || l.home_cost
 l.employee_code, 
 l.start_time, 
 l.end_time, 
-l.home_cost_center, 
-l.worked_cost_center, 
-t.title as employee_title,
-c.square as worked_cafe_name
-from file_uploads.cafe_labor_hours l
-left join file_uploads.employee_titles t on t.employee_code = l.employee_code
+l.employee_title,
+c.square as cafe_name
+from {{ref('labor_hours')}} l
 left join {{ref('cafe_mapping')}} c on c.store_number = l.worked_cost_center
