d4f0601789c157802c1a4d05249018bf2533dae2
Andres Recalde <andres.recalde@lacolombe.net>
2017-02-01 21:01:48 -0500

    fixed retail labor hours

--- models/file_uploads/labor_hours.sql
+++ models/file_uploads/labor_hours.sql
@@ -1,9 +1,17 @@
-select distinct md5(l.employee_code || l.start_time || l.end_time || l.home_cost_center || l.worked_cost_center) as unique_key, 
+select distinct md5(l.employee_code || l.worked_cost_center || "datetime" || round(datediff(minute, new_datetime, next_hour)::decimal(16,2) / 60, 2)) as unique_key, 
 l.employee_code, 
-l.start_time, 
-l.end_time, 
-l.home_cost_center, 
-l.worked_cost_center, 
+worked_cost_center, "datetime" as date_time, 
+round(datediff(minute, new_datetime, next_hour)::decimal(16,2) / 60, 2) as labor_hours,
 t.title as employee_title
+from (
+select *,
+case
+  when date_trunc('hour', start_time) = datetime then start_time
+  else datetime
+end as new_datetime,
+nvl(lead(datetime) over (partition by employee_code, worked_cost_center, start_time order by employee_code, worked_cost_center, datetime), end_time) as next_hour
 from file_uploads.cafe_labor_hours l
+left join file_uploads.hourly_calendar h on h.datetime >= date_trunc('hour', start_time)  and h.datetime <= end_time
+) l
 left join file_uploads.employee_titles t on t.employee_code = l.employee_code
+where datediff(minute, new_datetime, next_hour) > 0
--- models/tables/retail/retail_labor_hours.sql
+++ models/tables/retail/retail_labor_hours.sql
@@ -4,10 +4,10 @@
     "unique_key" : "unique_key"
     })
 }}
-select distinct md5(l.employee_code || l.start_time || l.end_time || l.home_cost_center || l.worked_cost_center) as unique_key, 
+select l.unique_key, 
 l.employee_code, 
-l.start_time, 
-l.end_time, 
+l.date_time, 
+l.labor_hours, 
 l.employee_title,
 c.square as cafe_name
 from {{ref('labor_hours')}} l
