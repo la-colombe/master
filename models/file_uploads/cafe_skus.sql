1d3ee74e512a3382a94ff6bb3a5dc12bd1a4f028
Andres Recalde <andres.recalde@lacolombe.net>
2017-02-12 19:46:03 -0500

    changed name to square

--- models/file_uploads/cafe_skus.sql
+++ models/file_uploads/cafe_skus.sql
@@ -6,5 +6,5 @@
 }}
 
 
-select square_name, sku, product_name, default_weekly_volume, ratio, date_trunc('day', start_date) as start_date
+select square_name as square, sku, product_name, default_weekly_volume, ratio, date_trunc('day', start_date) as start_date
 from google_sheets.cafe_skus 
