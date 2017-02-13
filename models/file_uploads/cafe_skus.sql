232700d0a7c8ac72a9f74a55eddf79b670190a65
Andres Recalde <andres.recalde@lacolombe.net>
2017-02-12 19:41:43 -0500

    added start_date to cafe_skus

--- models/file_uploads/cafe_skus.sql
+++ models/file_uploads/cafe_skus.sql
@@ -6,5 +6,5 @@
 }}
 
 
-select square_name, sku, product_name, default_weekly_volume, ratio, date_trunc('day', start_date)
+select square_name, sku, product_name, default_weekly_volume, ratio, date_trunc('day', start_date) as start_date
 from google_sheets.cafe_skus 
